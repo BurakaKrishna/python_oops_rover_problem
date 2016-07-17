3fb304a2a68955dd0ab8f2d2303d1f1c9869f230
srk <krishna.buraka@roadrunnr.in>
2016-07-17 23:33:08 +0530

    input can now be given from command prompt

--- robot_problem.py
+++ robot_problem.py
@@ -1,6 +1,7 @@
-class Grid(x,y):
-	self.length = x
-	self.breadth = y
+class Grid():
+	def __init__(self,x,y):
+		self.length = x
+		self.breadth = y
 class Rover():
 	class north():
 		left = 'W'
@@ -8,7 +9,7 @@ class Rover():
 		name = 'north'
 	class south():
 		left = 'E'
-		right = 'N'
+		right = 'W'
 		name = 'south'
 	class east():
 		left = 'N'
@@ -19,8 +20,8 @@ class Rover():
 		right = 'N'
 		name = 'west'
 	def __init__(self,x,y,compass):
-		self.x_position = x
-		self.y_position = y 
+		self.x_position = int(x)
+		self.y_position = int(y) 
 		self.facing = self.get_direction(compass)
 	def get_direction(self,compass):
 		if compass == 'N':
@@ -34,23 +35,23 @@ class Rover():
 	def rotate(self,input):
 		if input == 'L':
 			 self.facing = self.get_direction(self.facing.left)
-			 print 'new facing is %s' % self.facing.name
+			 # print 'new facing is %s' % self.facing.name
 		elif input == 'R':
 			 self.facing = self.get_direction(self.facing.right)
-			 print 'new facing is %s' % self.facing.name
+			 # print 'new facing is %s' % self.facing.name
 	def move(self):
 		if self.facing.name == 'north':
 			self.y_position += 1
-			print 'new y position is %s and new x position is %s' % (self.y_position,self.x_position)
+			# print 'new y position is %s and new x position is %s' % (self.y_position,self.x_position)
 		elif self.facing.name == 'south':
 			self.y_position -= 1
-			print 'new y position is %s and new x position is %s' % (self.y_position,self.x_position)
+			# print 'new y position is %s and new x position is %s' % (self.y_position,self.x_position)
 		elif self.facing.name == 'east':
 			self.x_position += 1
-			print 'new y position is %s and new x position is %s' % (self.y_position,self.x_position)
+			# print 'new y position is %s and new x position is %s' % (self.y_position,self.x_position)
 		else:
 			self.x_position -= 1
-			print 'new y position is %s and new x position is %s' % (self.y_position,self.x_position)
+			# print 'new y position is %s and new x position is %s' % (self.y_position,self.x_position)
 	def locomote(self,command):
 		if command == 'M':
 			self.move()
@@ -59,6 +60,28 @@ class Rover():
 	def locomotion(self,input_string=[]):
 		list_of_commands = list(input_string)
 		for command in list_of_commands:
-			print command
+			# print command
 			self.locomote(command)
 			print 'Rover x coordinate is %s, y_coordinate is %s,facing is %s' %(self.x_position,self.y_position,self.facing.name)
+
+def main():
+	grid_length = raw_input('Enter the grid length ')
+	grid_breadth = raw_input('Enter the grid breadth ')
+	grid = Grid(grid_length,grid_breadth)
+	print 'Grid is created'
+	no_of_rovers = raw_input('Enter how many rovers u need ')
+	Rovers_list = []
+	for each in range(int(no_of_rovers)):
+		x = raw_input('Enter the x coordinates of Rover ')
+		y = raw_input('Enter the y coordinates of Rover ')
+		facing = raw_input('Enter the facing direction of Rover ')
+		rover = Rover(x,y,facing)
+		Rovers_list.append(rover)
+	for rover in Rovers_list:
+		count = 1
+		input_string = raw_input('Enter the input_string for rover %s to go ' % count)
+		rover.locomotion(input_string)
+		count += 1
+	
+if __name__ == '__main__':
+	main()
