545abb2d2ccce8c21da6ed96e623c5be4ac6e86f
srk <krishna.buraka@roadrunnr.in>
2016-07-18 12:05:05 +0530

    adding test cases

---
+++ rover_test_cases.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestRover(unittest.Testcase):
+	
+	def test_rover_initialization_with_string_x_and_y():
+
+
+	def test_rover_initialization_with_int_N():
+
+	def test_rover_initializaion_test
